7dcfa9b38c1dbc3a298974d731ad9db585bb8d1b
Sara Harvey <saraharveynyc@gmail.com>
2018-09-02 22:28:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-friends = ["Bob", "Carol", "Sue"]
-friends.unshift("Hope")
+["Bob", "Carol", "Sue"]
+array.unshift("Hope")
 end
 
 def remove_element_from_end_of_array(array)
